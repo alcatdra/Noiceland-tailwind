1791dbc546a701eee89f68e9c22814caaa3f8d1b
alcatdra <alexander.menko.reggie@gmail.com>
2024-07-12 12:49:59 +0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,54 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    screens: {
+      lg: '1200px',
+      md: '990px',
+      sm: '600px',
+    },
+    extend: {
+      fontFamily: {
+        tenorSans: ['Tenor Sans', 'sans-serif'],
+        libreFranklin: ['Libre Franklin', 'sans-serif'],
+        sourceSerif: ['"Source Serif 4"', 'serif'],
+      },
+      colors: {
+        hrGrey: '#E6E6E6',
+        lightGrey: '#999999',
+        grey: '#666666',
+        footerGrey: '#141414',
+      },
+      fontSize: {
+        xl: '36px',
+        category: [
+          '10px',
+          {
+            lineHeight: '12px',
+            letterSpacing: '1px',
+            fontWeight: '400',
+          },
+        ],
+        title: [
+          '20px',
+          {
+            lineHeight: '24px',
+            fontWeight: '400',
+          },
+        ],
+        footerCategory: [
+          '14px',
+          {
+            lineHeight: '24px',
+            fontWeight: '400',
+          },
+        ],
+      },
+      width: {
+        contentPicture: '387px',
+        latestPostsPicture: '285px',
+      },
+    },
+  },
+  plugins: [],
+};
